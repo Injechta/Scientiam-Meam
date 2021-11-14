5778ff9277ac97a3ef8ce5db00b6bf99d2c4b763
Gregory ELBAJOURY <34866703+Injechta@users.noreply.github.com>
2021-11-14 20:35:41 +0100

    Add files via upload
    startin responsive

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
     $('.menu-toggle').on('click', function() {
         $('.nav').toggleClass('showing');
+        $('.nav ul').toggleClass('showing');
+        $('.nav ul li').toggleClass('showing');
     });
 
 });
