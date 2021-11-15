43d17687c01b741b9eab0a9dde990d8352749140
Gregory ELBAJOURY <34866703+Injechta@users.noreply.github.com>
2021-11-15 23:09:58 +0100

    Add files via upload
    Starting implementing caroussel

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,15 @@ $(document).ready(function() {
         $('.nav ul').toggleClass('showing');
         $('.nav ul li').toggleClass('showing');
     });
+    /* Slick Caroussel */
+    $('.post-wrapper').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: $('.prev'),
+        nextArrow: $('.next'),
+      });
+      
 
 });
